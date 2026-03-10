1597bd95bfd65ebc968d4e281386bfa33657aab9
anish69893-collab <anish69893@gmail.com>
2026-03-10 23:59:15 +0530

    PROGRAM.PY

--- input.py
+++
@@ -1,3 +0,0 @@
-first=int(input("enter the first number:"))
-second=int(input("enter the second number:"))
-print ("sum", first+second)
--- loop.java
+++
@@ -1,9 +0,0 @@
-public class loop {
- public static void main(String[] args) {
-  int i=0;
-  while(i<=9){
-    System.out.println(i+"");
-    i++;
- }
-  }
-}
