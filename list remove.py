b08401db10dbf5dff699065b4aa91a8ca67e871f
anish69893-collab <anish69893@gmail.com>
2026-03-11 21:29:36 +0530

    anish

---
+++ list remove.py
@@ -0,0 +1,8 @@
+
+list=[2,3,8,3,2]
+print(list.pop(3))
+print(list)
+
+list=[2,4,3,4,2]
+print(list.remove(2))
+print(list)
---
+++ tuples.py
@@ -0,0 +1,8 @@
+tup=(1,4,3,6)
+print(type(tup))
+print(tup[0])
+print(tup[2])
+
+tup=(1,)
+print(type(tup))
+print(tup)
