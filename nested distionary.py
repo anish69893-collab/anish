bd34bd25e08d4c17368b8734ab04d13167ae7d57
anish69893-collab <anish69893@gmail.com>
2026-03-15 18:47:51 +0530

    python

---
+++ nested distionary.py
@@ -0,0 +1,18 @@
+Student={
+    "Name":"Anish kumar",
+    "age":19,
+    "School name":"Sanskriti Internation School",
+        "District":"Siwan",
+        "police station":"Darwli",
+        "Roll no":3,       
+    "subject":{
+        "Phy":98,
+        "Chem":89,
+        "Maths":99,
+         
+
+    }
+    
+}
+print(Student["subject"])
+print(Student) 
