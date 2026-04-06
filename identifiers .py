10697138039953b0cb08f1496b5c4b2c36c7d22e
anish69893-collab <anish69893@gmail.com>
2026-04-06 10:00:05 +0530

    identifiers

---
+++ identifiers .py
@@ -0,0 +1,13 @@
+name= "Anish kumar gond "
+age=20
+marks="99%"
+price=77.9
+list=[2,3,6,7,7,8]
+i=(2,5,6,7,8,8,)
+a=None
+print(type(name))
+print(type(marks))
+print(type(price))
+print(type(list))
+print(type(i))
+print(type(None))
