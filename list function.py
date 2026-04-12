256da9672e0ea31ca6b758c377d89d2d2d0a5780
anish69893-collab <anish69893@gmail.com>
2026-04-12 14:17:44 +0530

    function

---
+++ list function.py
@@ -0,0 +1,8 @@
+boys=["Anish","Aman","Amit","ajit",]
+girls=["Manvi","Neha","Suman",]
+
+def print_len(list):
+    print(len(list))
+    return list
+print_len(boys)
+print_len(girls)
---
+++ list in function.py
@@ -0,0 +1,9 @@
+Boys=["anish","amit","ajit","sujit","aditya","rajnish","ankit","manish"]
+
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+print_list(Boys)
