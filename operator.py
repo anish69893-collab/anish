dd0496a3aa81062f365e42b90bf2b6622c4bba50
anish69893-collab <anish69893@gmail.com>
2026-06-28 15:27:06 +0530

    pyhton

---
+++ operator.py
@@ -0,0 +1,20 @@
+# Arithmetic_operator
+a=6
+b=5
+sum=(a+b)
+print(sum)
+
+# Relational_operator
+print(a==b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+print(ab=b)
+
+# Assignment_operator
+num=10;
+num=10/num
+num+=10
+print("num",num)
+
