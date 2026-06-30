61414ad2543e5409f25c8df84427999e3c774bec
anish69893-collab <anish69893@gmail.com>
2026-06-30 09:55:03 +0530

    python

---
+++ indexing.py
@@ -0,0 +1,15 @@
+str1="this is a pyhton.\n we are creating it  in python."
+print(len(str1))
+print(str1)
+print(str1[2])
+print(str1[8])
+print(str1[10])
+print(str1[11])
+
+str2="this is a Anish kumar.\t he is student."
+print(str2)
+print(str2[14])
+print(str2[19])
+len=len(str2)
+print(type(str1))
+print(type(str2))
