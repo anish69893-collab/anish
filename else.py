fc4e8edcfb794196d26f162ac263519ca86a6195
anish69893-collab <anish69893@gmail.com>
2026-07-03 22:02:04 +0530

    python

---
+++ else.py
@@ -0,0 +1,6 @@
+age=2
+if(age>=18):
+    print("can vote")
+else:
+    print("cannot vote")
+
---
+++ ne.py
@@ -0,0 +1,4 @@
+str="Hi,$ i am the $ symbol $99,$98"
+print(str.count("$"))
+print(len(str))
+print(str[0:9])
