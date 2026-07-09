1cbb281b6bdd563f4951d71cbecd47aee4730e8b
anish69893-collab <anish69893@gmail.com>
2026-07-09 21:16:54 +0530

    python

---
+++ dic.py
@@ -0,0 +1,9 @@
+inf={
+    "name":"Anish",
+    "erp no" : 39095,
+    "collage name":"VGU",
+    "subject": [ "python" "java"],
+    "marks": ("99.9" "98.9")
+    }
+print(inf)
+print(type(inf))
---
+++ student dic.py
@@ -0,0 +1,12 @@
+student={
+    "Name": "Manvi kumari",
+    "Roll no":3,
+    "Collage": "Vivekananda Global University,jaipur",
+    "Marks student":{
+        "python":99.9,
+        "Java":98.9,
+        "c++":95.5,
+        }
+    }
+print(type(student))
+print(student)
