bf3ca9d827a22b5a1b4b9a40da141bea7a7a9645
anish69893-collab <anish69893@gmail.com>
2026-08-25 22:24:10 +0530

    python

---
+++ problem.py
@@ -0,0 +1,4 @@
+a=int(input("Enter number 1"))
+b=int(input("Enter number 2"))
+print("Number a is:",a )
+print("Number b is:",b)
---
+++ problem2.py
@@ -0,0 +1,21 @@
+# Logical Operators
+a= True or False
+
+# Truth table of 'OR'
+print("True or False is", True or False)
+print("True or True is", True or False)
+print("False or True is", True or False)
+print("False or False is",False or False)
+
+
+# Truth table of 'AND'
+print("True and False is", True and False)
+print("True and True is", True and False)
+print("False and True is", True and False)
+print("False and False is",False and False)
+
+
+# Truth table of 'NOT'
+print(not(False))
+print(not(True))
+
