913b604d186987797ee70f3506ce437aa7d843c2
anish69893-collab <anish69893@gmail.com>
2026-09-03 22:56:51 +0530

    python

---
+++ problem9.py
@@ -0,0 +1,13 @@
+fruits =[ "Apple", "Mango", "Orange", "Banana",]
+for fruits1 in fruits:
+    print(fruits) 
+    print(type(fruits))
+    print(len(fruits))
+    fruits.append("Grapes")
+    print(fruits)
+    fruits.sort()
+    print(fruits)
+    fruits.remove("Apple")
+    print(fruits)
+    fruits.reverse()
+    print(fruits)
