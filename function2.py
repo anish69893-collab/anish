3c5caa52193c35dfb2d35aa45c97ce8d3332f917
anish69893-collab <anish69893@gmail.com>
2026-09-06 22:54:56 +0530

    python

---
+++ function2.py
@@ -0,0 +1,13 @@
+def calc_avg(a,b,c):
+    Gmean=(a+b+c)/(a*b*c)
+    avg=Gmean/8
+    print(avg)
+    return avg
+calc_avg(68,67,65)
+
+# More line of code
+calc_avg(98,34,56)
+print(type(calc_avg))
+
+
+
