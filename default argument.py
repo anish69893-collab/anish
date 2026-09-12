8aad250c65465db2aea52ddcf323311dd3d62313
anish69893-collab <anish69893@gmail.com>
2026-09-12 22:16:46 +0530

    python

--- default argument.py
+++ default argument.py
@@ -1,3 +1,4 @@
+# default argument
 def name( fname,mname="Rahul", lname="Whatname"):
     print("hello", fname,mname,lname)
 name("Rohan", "Agrawal","Soni")
@@ -7,3 +8,15 @@ def calc_Average(a=98,b=90):
     print("The average", a+b/9)
 
 calc_Average(b=78)
+
+# Keyword argument
+def calc_Average(a=98,b=90):
+    print("The average", a+b/9)
+
+calc_Average(b=78 ,a=67)
+
+# Required argument
+def calc_Average(a,b=90):
+    print("The average", a+b/9)
+
+calc_Average(a=20)
